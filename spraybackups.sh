0a0984eea841bd7cf1f117d6c3df81b88a505eeb
Local Guard <localguard@localhost.localdomain>
2021-10-21 18:26:27 -0400

    final touches

--- spraybackups.sh
+++ spraybackups.sh
@@ -2,16 +2,16 @@
 # Spread backups out into multiple locations
 # Alex Collom: Hibobjr#3245
 mkdir -p /home/localguard/Documents/dankmemes
-cp /var/backups /home/localguard/Documents/dankmemes
+cp /var/backups /home/localguard/Documents/dankmemes -r
 
 mkdir -p /var/tmp/backups
-cp /var/backups /var/tmp/backups
+cp /var/backups /var/tmp/backups -r
 
 mkdir -p /backups
-cp /var/backups /backups
+cp /var/backups /backups -r
 
 mkdir -p /home/root/default/system/reserved/systemctl-misc/cache
-cp /var/backups /home/root/default/system/reserved/systemctl-misc/cache
+cp /var/backups /home/root/default/system/reserved/systemctl-misc/cache -r
 
 mkdir -p /root/tmp/files
-cp /var/backups /root/tmp/files
+cp /var/backups /root/tmp/files -r
